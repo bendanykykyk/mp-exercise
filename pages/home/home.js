cb65eba4b620b31928e563d07543754191510903
bendanykykyk <446679223@qq.com>
2022-11-24 19:43:21 +0800

    数据绑定/数据循环/嵌套循环/监听事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,9 +5,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      title:'大标题',
+      persons:[
+        {name:'郁开',age:18},
+        {name:'郁开2',age:18},
+        {name:'郁开3',age:18},
+      ],
+      lists:[
+        {title:"标题1",numbers:[1,2,3,4]},
+        {title:"标题2",numbers:[2,3,4,5]},
+        {title:"标题3",numbers:[1,2,3,4]}
+      ],
+      counter:0
   },
-
+  // 事件
+  handleAdd(){
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  handleReduce(){
+    this.setData({
+      counter:this.data.counter-1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
